f4da689e4373dcb89fa42b5aa0e38d4c6778c648
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-06 23:30:44 +0100

    tailwind

--- src/clientcomponent/ClientRestaurants.js
+++ src/clientcomponent/ClientRestaurants.js
@@ -1,13 +1,10 @@
-
 import axios from '../service/callerService';
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantDetails from "./RestaurantDetails";
 import { Link, useParams } from 'react-router-dom';
 import { AiOutlineFieldTime } from "react-icons/ai";
-
-
-
-
+import { Dropdown } from 'primereact/dropdown';
+import Button from "@mui/material/Button";
 
 export default function ClientRestaurants() {
     const [restaurants, setRestaurants] = useState([]);
@@ -16,108 +13,132 @@ export default function ClientRestaurants() {
     const [zones, setZones] = useState([]);
     const [selectedCity, setSelectedCity] = useState(null);
     const [selectedZone, setSelectedZone] = useState(null);
-
+    const [selectedSpecialite, setSelectedSpecialite] = useState(null);
+    const [specialites, setSpecialites] = useState([]);
+    const [noResults, setNoResults] = useState(false); // State for no results message
 
     useEffect(() => {
-        axios.get("/api/controller/restaurants/").then((response) => {
-            setRestaurants(response.data);
+        axios.get("/api/controller/specialites/").then((response) => {
+            setSpecialites(response.data);
         });
-    }, []);
-
-
-    useEffect(() => {
         axios.get("/api/controller/villes/").then((response) => {
             setCities(response.data);
         });
+        loadRestaurants();
+
     }, []);
 
-    // Fetch zones for the selected city
+    const loadRestaurants = () => {
+        axios.get("/api/controller/restaurants/").then((response) => {
+            setRestaurants(response.data);
+        });
+    };
+
     useEffect(() => {
         if (selectedCity) {
-            axios
-                .get(`/api/controller/zones/${selectedCity}`)
-                .then((response) => {
-                    setZones(response.data);
-                });
+            axios.get(`/api/controller/zones/${selectedCity}`).then((response) => {
+                setZones(response.data);
+            });
         }
     }, [selectedCity]);
 
-    // Fetch restaurants for the selected city and zone
     useEffect(() => {
-        if (selectedCity && selectedZone) {
-            axios
-                .get(
-                    `/api/controller/restaurants/${selectedCity}/${selectedZone}`
-                )
-                .then((response) => {
-                    setRestaurants(response.data);
-                });
+        if (selectedCity && selectedZone && selectedSpecialite) {
+            axios.get(
+                `/api/controller/restaurants/${selectedCity}/${selectedZone}/${selectedSpecialite}`
+            ).then((response) => {
+                setRestaurants(response.data);
+                setNoResults(response.data.length === 0);
+            });
+        } else {
+            setNoResults(false);
         }
-    }, [selectedCity, selectedZone]);
+    }, [selectedCity, selectedZone, selectedSpecialite]);
 
-    // Handle city select
     const handleCityChange = (event) => {
-        setSelectedCity(event.target.value);
-        setSelectedZone("");
+        setSelectedCity(event.value);
+        loadRestaurants();
+        setSelectedZone(null);
+        setSelectedSpecialite(null);
     };
+
 
     const handleZoneChange = (event) => {
-        setSelectedZone(event.target.value);
+        setSelectedZone(event.value);
     };
+
+    const handleSpecialiteChange = (event) => {
+        setSelectedSpecialite(event.value);
+    };
+
 
 
     return (
-        <div className="container mt-5">
+        <div className="container mt-5 ">
+            <div className="col-md-12   mb-3">
+                <Dropdown
+                    className="mr-2"
+                    value={selectedCity}
+                    options={cities.map((ville) => ({ label: ville.nom, value: ville.nom }))}
+                    onChange={handleCityChange}
+                    placeholder="Select a city"
+                />
 
-            <div className="col-md-6 d-flex">
-                <select className="form-select mb-3 me-3" value={selectedCity} onChange={handleCityChange}>
-                    <option value="">Select a city</option>
-                    {cities.map((ville) => (
-                        <option key={ville.id} value={ville.nom}>{ville.nom}</option>
-                    ))}
-                </select>
-                {selectedCity ? (
-                    <select className="form-select mb-3" value={selectedZone} onChange={handleZoneChange}>
-                        <option value="">Select a zone</option>
-                        {zones.map((zone) => (
-                            <option key={zone.id} value={zone.nom}>{zone.nom}</option>
-                        ))}
-                    </select>
-                ) : (
-                    <select className="form-select mb-3" disabled>
-                        <option value="">No zone</option>
-                    </select>
+                {selectedCity && (
+                    <Dropdown
+                        value={selectedZone}
+                        options={zones.map((zone) => ({ label: zone.nom, value: zone.nom }))}
+                        onChange={handleZoneChange}
+                        placeholder="Select a zone"
+                    />
                 )}
+
+                <Dropdown
+                    className="ml-2"
+                    value={selectedSpecialite}
+                    options={specialites.map((specialite) => ({ label: specialite.nom, value: specialite.nom }))}
+                    onChange={handleSpecialiteChange}
+                    placeholder="Select a specialite"
+                />
             </div>
 
-
-
-
             <div className="row row-cols-2 row-cols-md-2 row-cols-lg-4 g-4">
-                {restaurants.map((restaurant) => (
-                    <div key={restaurant.id} className="col mb-4">
+                {noResults ? (
+                    <div className="col mb-4">
                         <div className="card h-100">
-                            <Link to={`restaurants/${restaurant.id}`}>
-                                <img
-                                    src={restaurant.photo}
-                                    className="card-img-top"
-                                    alt="Restaurant"
-                                    style={{ objectFit: "cover", height: "auto" }}
-                                />
-                            </Link>
                             <div className="card-body">
-                                <h6 className="card-title">{restaurant.nom}</h6>
-                                <h6 className="card-title " ><AiOutlineFieldTime style={{ color: 'lightseagreen', fontSize: '20px' }} />
-                                    {restaurant.dateOuverture} / {restaurant.dateFermeture}</h6>
-                                <h6 className="card-text">Address: {restaurant.adresse}</h6>
+                                <h6 className="card-title">No results found</h6>
                             </div>
                         </div>
                     </div>
-                ))}
+                ) : (
+                    restaurants.map((restaurant) => (
+                        <div key={restaurant.id} className="col mb-4">
+                            <div className="card h-100">
+                                <Link to={`restaurants/${restaurant.id}`}>
+                                    <img
+                                        src={restaurant.photo}
+                                        className="card-img-top"
+                                        alt="Restaurant"
+                                        style={{ objectFit: "cover", height: "auto" }}
+                                    />
+                                </Link>
+                                <div className="card-body">
+                                    <h6 className="card-title">{restaurant.nom}</h6>
+                                    <h6 className="card-title">
+                                        <AiOutlineFieldTime style={{ color: 'lightseagreen', fontSize: '20px' }} />
+                                        {restaurant.dateOuverture} / {restaurant.dateFermeture}
+                                    </h6>
+                                    <h6 className="card-text">Address: {restaurant.adresse}</h6>
+                                    <h6 className="card-text">specialite: {restaurant.specialite.nom}</h6>
+                                    <h6 className="card-text">zone: {restaurant.zone.nom}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
             {id && <RestaurantDetails id={id} />}
         </div>
-
     );
 }
-
