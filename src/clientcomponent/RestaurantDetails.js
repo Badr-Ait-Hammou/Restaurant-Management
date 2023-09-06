f56a97f43df1ea861db0a80f175e0e40d1553096
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-06 20:58:39 +0100

    prime react product cart

--- src/clientcomponent/RestaurantDetails.js
+++ src/clientcomponent/RestaurantDetails.js
@@ -1,12 +1,11 @@
 import {Link, useParams} from 'react-router-dom';
 import axios from '../service/callerService';
-import { useEffect, useState,useRef } from "react";
-import {Card, CardContent} from "@mui/material";
-import {Col,Row} from "react-bootstrap";
+import {useEffect, useState, useRef} from "react";
+import {Card, CardContent, Rating} from "@mui/material";
+import {Col, Row} from "react-bootstrap";
 import React from "react";
 import {accountService} from "../service/accountService";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Button } from 'primereact/button';
+import {Button} from 'primereact/button';
 import {Toast} from "primereact/toast";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import 'primeicons/primeicons.css';
@@ -17,12 +16,13 @@ import {Tag} from "primereact/tag";
 export default function RestaurantDetails() {
     const [longitude, setLongitude] = useState();
     const [latitude, setLatitude] = useState();
-    const { id } = useParams();
+    const {id} = useParams();
     const [restaurant, setRestaurant] = useState(null);
     const [products, setProducts] = useState([]);
     const [userId, setUserId] = useState("");
     const toast = useRef(null);
     const [productInCart, setProductInCart] = useState({});
+
 
     const loadProductsUser = () => {
         const checkProductInCart = (productId) => {
@@ -56,7 +56,7 @@ export default function RestaurantDetails() {
                 try {
                     const user = await accountService.getUserByEmail(tokenInfo.sub);
                     setUserId(user.id);
-                    console.log('user',user.id);
+                    console.log('user', user.id);
                 } catch (error) {
                     console.log('Error retrieving user:', error);
                 }
@@ -64,12 +64,6 @@ export default function RestaurantDetails() {
         };
         fetchUserData();
     }, []);
-
-
-
-
-
-
 
 
     const handleAddToCart = (product) => {
@@ -86,9 +80,9 @@ export default function RestaurantDetails() {
 
         axios.post('/api/controller/carts/', cartItem)
             .then(response => {
-                    console.log('Product added to cart successfully!');
-                    showSuccess();
-                    loadProductsUser();
+                console.log('Product added to cart successfully!');
+                showSuccess();
+                loadProductsUser();
             })
             .catch(error => {
                 console.error('Error adding product to cart:', error);
@@ -126,125 +120,153 @@ export default function RestaurantDetails() {
     }
 
     const showSuccess = () => {
-        toast.current.show({severity:'success', summary: 'Success', detail:'item added to cart', life: 1000});
+        toast.current.show({severity: 'success', summary: 'Success', detail: 'item added to cart', life: 1000});
     }
 
+
+    const itemTemplate = (product) => {
+        if (!product) {
+            return;
+        }
+        return (
+            <div key={product.id} className={`col mb-4 ${product.stock <= 0 ? 'out-of-stock' : ''}`}>
+                <div className="card h-100">
+                    <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+                        <Link to={`products/${product.id}`}>
+                            <div style={{position: 'relative'}}>
+                                <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"
+                                     src={product.photo}
+                                     alt={product.nom}
+                                     style={{
+                                         width: '100px',
+                                         height: '100px',
+                                         borderRadius: '8px'
+                                     }}/>
+                                {product.stock <= 0 ? (
+                                    <Tag
+                                        severity="warning"
+                                        value="Out of Stock"
+                                        className="stock-tag"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '10px',
+                                            right: '10px',
+                                        }}
+                                    />
+                                ) : (
+                                    <Tag
+                                        severity="success"
+                                        value="In Stock"
+                                        className="stock-tag"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '10px',
+                                            right: '10px',
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        </Link>
+                        <div
+                            className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+
+                                <div className="text-2xl font-bold text-900">{product.nom}</div>
+                                <Rating value={product.id} readOnly cancel={false}></Rating>
+                                <div className="flex align-items-center gap-3">
+                                <span className="flex align-items-center gap-2">
+                                    <i className="pi pi-tag"></i>
+                                    <span className="font-semibold">{product.prix}</span>
+                                </span>
+                                    <Tag value={product.stock}></Tag>
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-lg-between gap-3 align-items-center mt-3">
+                                <span className="text-2xl font-semibold">${product.prix}</span>
+
+                                {productInCart[product.id] ? (
+                                    <Link to="/admin/cart">
+                                        <Button
+                                            icon="pi pi-external-link"
+                                            className="p-button-rounded mt-2"
+                                            disabled={product.stock <= 0}
+                                        />
+                                    </Link>
+                                ) : (
+                                    <Button
+                                        icon="pi pi-shopping-cart"
+                                        className="p-button-rounded mt-2"
+                                        onClick={() => handleAddToCart(product)}
+                                        disabled={product.stock <= 0 || productInCart[product.id]}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+
+    const groupedProducts = [];
+    for (let i = 0; i < products.length; i += 4) {
+        groupedProducts.push(products.slice(i, i + 4));
+    }
 
 
     return (
         <>
             <Toast ref={toast}/>
 
-            <Button  icon="pi pi-shopping-cart"
-                     raised
-                     className="mx-2 mt-2"  style={{backgroundColor:"transparent",color:"lightseagreen",fontSize:"20px"}} />
+            <Button icon="pi pi-shopping-cart"
+                    raised
+                    className="mx-2 mt-2"
+                    style={{backgroundColor: "transparent", color: "lightseagreen", fontSize: "20px"}}/>
 
-        <Card className="mt-3 mx-2" style={{backgroundColor:"whitesmoke"}}>
-            <CardContent>
-                <Row>
-                    <Col sm={12} md={6} className="mb-3 mb-md-0">
-                        <div className="justify-content-center d-flex">
-                            <img alt="badr" src={restaurant.photo}  style={{width: "70%",
-                                height: "300px",objectFit:"fill",borderRadius:"20px"}} />
-                        </div>
-                        <div className="details-container" style={{  padding: '10px', marginBottom: '10px' }}>
-                            <h3 style={{ fontFamily: 'sans-serif', fontSize: '40px', marginBottom: '20px', color: '#20b0a8' }}>{restaurant.nom}</h3>
-                            <strong style={{ fontSize: '18px', color: '#181818' }}>Address: {restaurant.adresse}</strong>
-                        </div>
-                        <div className="details-container" style={{  padding: '10px' }}>
-                            <strong className="card-title" style={{ fontSize: '18px', color: '#333' }}>OPEN : {restaurant.dateOuverture} / {restaurant.dateFermeture}</strong>
-                        </div>
-
-
-                    </Col>
-                    <Col sm={12} md={6}>
-                        <div className="map-container justify-content-center d-flex">
-                            <iframe id="iframeId" height="450px" width="80%" title="Example website" style={{borderRadius:"20px"}}></iframe>
-                        </div>
-                    </Col>
-                </Row>
-
-            </CardContent>
-        </Card>
-
-
-            <div className="container mt-5">
-                <div className="row row-cols-2 row-cols-md-2 row-cols-lg-4 g-4">
-                    {products.map((product) => (
-                        <div key={product.id} className={`col mb-4 ${product.stock <= 0 ? "out-of-stock" : ""}`}>
-                            <div className="card h-100">
-                                <Link to={`products/${product.id}`}>
-                                    <div style={{ position: "relative" }}>
-                                        <img
-                                            src={product.photo}
-                                            className="card-img-top"
-                                            alt="rest"
-                                            style={{ objectFit: "cover", height: "auto" }}
-                                        />
-                                        {product.stock <= 0 ? (
-                                            <Tag
-                                                severity="warning"
-                                                value="Out of Stock"
-                                                className="stock-tag"
-                                                style={{
-                                                    position: "absolute",
-                                                    top: "10px",
-                                                    right: "10px",
-                                                }}
-                                            />
-                                        ) : (
-                                            <Tag
-                                                severity="success"
-                                                value="In Stock"
-                                                className="stock-tag"
-                                                style={{
-                                                    position: "absolute",
-                                                    top: "10px",
-                                                    right: "10px",
-                                                }}
-                                            />
-                                        )}
-                                    </div>
-                                </Link>
-                                <div className="card-body">
-                                    <h5 className="card-title">{product.nom}</h5>
-                                    <p className="card-text">Prix: {product.prix}</p>
-                                    <p className="card-text">Stock: {product.stock}</p>
-
-                                    <Button
-                                        raised
-                                        className="mx-2"
-                                        onClick={() => handleAddToCart(product)}
-                                        disabled={product.stock <= 0 || productInCart[product.id]}
-                                        style={{
-                                            backgroundColor: productInCart[product.id] ? 'lightseagreen' : 'lightgreen',
-                                            cursor: product.stock <= 0 || productInCart[product.id] ? 'not-allowed' : 'pointer',
-                                            alignItems: 'center',
-                                            justifyContent: 'center',
-                                            gap: '0.5rem',
-                                            padding: '0.5rem',
-                                            borderRadius: '4px',
-                                            border: 'none',
-                                            color: '#fff',
-                                        }}
-                                    >
-                                        {productInCart[product.id] ? (
-                                            <>
-                                                <ShoppingCartIcon />
-                                                Added
-                                            </>
-                                        ) : (
-                                            <>
-                                                <ShoppingCartIcon />
-                                                Add to Cart
-                                            </>
-                                        )}
-                                    </Button>
-                                </div>
+            <Card className="mt-3 mx-2" style={{backgroundColor: "whitesmoke"}}>
+                <CardContent>
+                    <Row>
+                        <Col sm={12} md={6} className="mb-3 mb-md-0">
+                            <div className="justify-content-center d-flex">
+                                <img alt="badr" src={restaurant.photo} style={{
+                                    width: "70%",
+                                    height: "300px", objectFit: "fill", borderRadius: "20px"
+                                }}/>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                            <div className="details-container" style={{padding: '10px', marginBottom: '10px'}}>
+                                <h3 style={{
+                                    fontFamily: 'sans-serif',
+                                    fontSize: '40px',
+                                    marginBottom: '20px',
+                                    color: '#20b0a8'
+                                }}>{restaurant.nom}</h3>
+                                <strong
+                                    style={{fontSize: '18px', color: '#181818'}}>Address: {restaurant.adresse}</strong>
+                            </div>
+                            <div className="details-container" style={{padding: '10px'}}>
+                                <strong className="card-title" style={{fontSize: '18px', color: '#333'}}>OPEN
+                                    : {restaurant.dateOuverture} / {restaurant.dateFermeture}</strong>
+                            </div>
+
+
+                        </Col>
+                        <Col sm={12} md={6}>
+                            <div className="map-container justify-content-center d-flex">
+                                <iframe id="iframeId" height="450px" width="80%" title="Example website"
+                                        style={{borderRadius: "20px"}}></iframe>
+                            </div>
+                        </Col>
+                    </Row>
+
+                </CardContent>
+            </Card>
+            <div className="container mt-5">
+                {groupedProducts.map((group) => (
+                    <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                        {group.map((product) => itemTemplate(product))}
+                    </div>
+                ))}
             </div>
         </>
     );
