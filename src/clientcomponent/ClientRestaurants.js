42a3859c4bfd4b330e0012ad07fa26695c6e07da
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-07 17:00:33 +0100

    closed / open time

--- src/clientcomponent/ClientRestaurants.js
+++ src/clientcomponent/ClientRestaurants.js
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom';
 import {Dropdown} from 'primereact/dropdown';
 import {Tag} from "primereact/tag";
 import {DataView, DataViewLayoutOptions} from "primereact/dataview";
+import AlertTitle from "@mui/material/AlertTitle";
+import Alert from "@mui/material/Alert";
 
 export default function ClientRestaurants() {
     const [restaurants, setRestaurants] = useState([]);
@@ -32,6 +34,7 @@ export default function ClientRestaurants() {
     const loadRestaurants = () => {
         axios.get("/api/controller/restaurants/").then((response) => {
             setRestaurants(response.data);
+            console.log(response.data);
         });
     };
 
@@ -73,6 +76,23 @@ export default function ClientRestaurants() {
     };
 
 
+    function isRestaurantOpen(openingTime, closingTime) {
+        const now = new Date();
+        const openTime = parseTimeString(openingTime);
+        const closeTime = parseTimeString(closingTime);
+
+        return now >= openTime && now <= closeTime;
+    }
+
+    function parseTimeString(timeString) {
+        const [hours, minutes] = timeString.split(':');
+        const now = new Date();
+        now.setHours(parseInt(hours, 10));
+        now.setMinutes(parseInt(minutes, 10));
+        return now;
+    }
+
+
     const listItem = (restaurant) => {
         return (
             <div className="flex flex-column xl:flex-row xl:align-items-start p-2 gap-4">
@@ -81,40 +101,61 @@ export default function ClientRestaurants() {
                     <div className="col mb-4">
                         <div className="card h-100">
                             <div className="card-body">
-                                <h6 className="card-title">No results found</h6>
+                                <Alert severity="info">
+                                    <AlertTitle>Info</AlertTitle>
+                                    <strong>oops...!</strong> — No Restaurant Found
+                                </Alert>
                             </div>
                         </div>
                     </div>
                 ) : (
-                        <div key={restaurant.id} className="col mb-4 card h-100">
-                            <div className="row  row-cols-1  row-cols-sm-4 row-cols-md-4 row-cols-lg-4 g-4 ">
-                                <Link to={`restaurants/${restaurant.id}`}>
-                                        <img
-                                            className="card-img-top mx-auto mt-2 "
-                                            src={restaurant.photo}
-                                            alt={restaurant.nom}
-                                            style={{
-                                                width: '180px',
-                                                height: '140px',
-                                                borderRadius: '8px'
-                                            }}/>
-                                </Link>
-                                <div className="card-body">
-                                    <h6 className="card-title">{restaurant.nom}</h6>
-                                    <Tag severity="success" icon="pi pi-clock">
-
-                                        {restaurant.dateOuverture} / {restaurant.dateFermeture}
-                                    </Tag>
-                                    <div>
-                                    <strong className="card-text">Address: </strong> {restaurant.adresse}
-                                    </div>
-                                    <div>
-                                    <strong className="card-text">specialite:</strong> {restaurant.specialite.nom}
-                                    <h6 className="card-text">zone: {restaurant.zone.nom}</h6>
-                                    </div>
+                    <div key={restaurant.id} className="col mb-4 card h-100">
+                        <div className="row  row-cols-1  row-cols-sm-4 row-cols-md-4 row-cols-lg-4 g-4 ">
+                            <Link to={`restaurants/${restaurant.id}`}>
+                                <img
+                                    className="card-img-top mx-auto mt-2 "
+                                    src={restaurant.photo}
+                                    alt={restaurant.nom}
+                                    style={{
+                                        width: '180px',
+                                        height: '140px',
+                                        borderRadius: '8px'
+                                    }}/>
+                            </Link>
+                            <div className="card-body">
+                                <h6 className="card-title">{restaurant.nom}</h6>
+                                <Tag severity="success" icon="pi pi-clock">
+                                    {restaurant.dateOuverture} / {restaurant.dateFermeture}
+                                </Tag>
+                                <span className="card-text-value mx-2">
+                                    {restaurant.dateOuverture && restaurant.dateFermeture ? (
+                                        isRestaurantOpen(restaurant.dateOuverture, restaurant.dateFermeture) ? (
+                                            <Tag severity="info" icon="pi pi-check">
+                                                Open
+                                            </Tag>
+                                        ) : (
+                                            <Tag severity="danger" icon="pi pi-moon">
+                                                Closed
+                                            </Tag>
+                                        )
+                                    ) : (
+                                        "N/A"
+                                    )}
+                                </span>
+                                <div className="mt-1">
+                                    <strong className="card-text ">Address: </strong> {restaurant.adresse}
                                 </div>
+                                <div>
+                                    <strong
+                                        className="card-text mt-1 ">City: </strong>{restaurant.zone.ville.nom}--{restaurant.zone.nom}
+                                </div>
+                                <div>
+                                    <strong className="card-text">Speciality:</strong> {restaurant.specialite.nom}
+                                </div>
+
                             </div>
                         </div>
+                    </div>
 
                 )}
             </div>
@@ -129,37 +170,62 @@ export default function ClientRestaurants() {
                     <div className="col mb-4">
                         <div className="card h-100">
                             <div className="card-body">
-                                <h6 className="card-title">No results found</h6>
+                                <Alert severity="info">
+                                    <AlertTitle>Info</AlertTitle>
+                                    <strong>oops...!</strong> — No Restaurant Found
+                                </Alert>
                             </div>
                         </div>
                     </div>
                 ) : (
-            <div key={restaurant.id} className="col mb-4 card h-100">
-                <div className="card p-2">
-                    <Link to={`restaurants/${restaurant.id}`}>
-                            <img
-                                className="w-90 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"
-                                src={restaurant.photo}
-                                alt={restaurant.nom}
-                                style={{
-                                    width: '180px',
-                                    height: '140px',
-                                    borderRadius: '8px',
-                                }}
-                            />
-                    </Link>
-                    <div className="card-body">
-                        <h6 className="card-title">{restaurant.nom}</h6>
-                        <Tag severity="success" icon="pi pi-clock">
+                    <div key={restaurant.id} className="col mb-4 card h-100">
+                        <div className="card p-2">
+                            <Link to={`restaurants/${restaurant.id}`}>
+                                <img
+                                    className="card-img-top mx-auto mt-2 "
+                                    src={restaurant.photo}
+                                    alt={restaurant.nom}
+                                    style={{
+                                        width: '180px',
+                                        height: '140px',
+                                        borderRadius: '8px',
+                                    }}
+                                />
+                            </Link>
+                            <div className="card-body">
+                                <h6 className="card-title">{restaurant.nom}</h6>
+                                <Tag severity="success" icon="pi pi-clock">
 
-                            {restaurant.dateOuverture} / {restaurant.dateFermeture}
-                        </Tag>
-                        <h6 className="card-text">Address: {restaurant.adresse}</h6>
-                        <h6 className="card-text">specialite: {restaurant.specialite.nom}</h6>
-                        <h6 className="card-text">zone: {restaurant.zone.nom}</h6>
+                                    {restaurant.dateOuverture} / {restaurant.dateFermeture}
+                                </Tag>
+                                <span className="card-text-value mx-2">
+                                    {restaurant.dateOuverture && restaurant.dateFermeture ? (
+                                        isRestaurantOpen(restaurant.dateOuverture, restaurant.dateFermeture) ? (
+                                            <Tag severity="info" icon="pi pi-check">
+                                                Open
+                                            </Tag>
+                                        ) : (
+                                            <Tag severity="danger" icon="pi pi-moon">
+                                                Closed
+                                            </Tag>
+                                        )
+                                    ) : (
+                                        "N/A"
+                                    )}
+                                </span>
+                                <div className="mt-1">
+                                    <strong className="card-text ">Address: </strong> {restaurant.adresse}
+                                </div>
+                                <div>
+                                    <strong
+                                        className="card-text mt-1 ">City: </strong>{restaurant.zone.ville.nom}--{restaurant.zone.nom}
+                                </div>
+                                <div>
+                                    <strong className="card-text">Speciality:</strong> {restaurant.specialite.nom}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
                 )}
             </div>
         );
@@ -185,33 +251,33 @@ export default function ClientRestaurants() {
 
     return (
         <>
-                <div className="col-md-12   mb-3 mt-5">
+            <div className="col-md-12   mb-3 mt-5">
+                <Dropdown
+                    className="mr-2"
+                    value={selectedCity}
+                    options={cities.map((ville) => ({label: ville.nom, value: ville.nom}))}
+                    onChange={handleCityChange}
+                    placeholder="Select a city"
+                />
+                {selectedCity && (
                     <Dropdown
-                        className="mr-2"
-                        value={selectedCity}
-                        options={cities.map((ville) => ({label: ville.nom, value: ville.nom}))}
-                        onChange={handleCityChange}
-                        placeholder="Select a city"
+                        value={selectedZone}
+                        options={zones.map((zone) => ({label: zone.nom, value: zone.nom}))}
+                        onChange={handleZoneChange}
+                        placeholder="Select a zone"
                     />
-                    {selectedCity && (
-                        <Dropdown
-                            value={selectedZone}
-                            options={zones.map((zone) => ({label: zone.nom, value: zone.nom}))}
-                            onChange={handleZoneChange}
-                            placeholder="Select a zone"
-                        />
-                    )}
+                )}
 
-                    <Dropdown
-                        className="ml-2"
-                        value={selectedSpecialite}
-                        options={specialites.map((specialite) => ({label: specialite.nom, value: specialite.nom}))}
-                        onChange={handleSpecialiteChange}
-                        placeholder="Select a specialite"
-                    />
-                </div>
-            <div className="card mx-2" >
-                <DataView value={restaurants} itemTemplate={itemTemplate} layout={layout} header={header()} />
+                <Dropdown
+                    className="ml-2"
+                    value={selectedSpecialite}
+                    options={specialites.map((specialite) => ({label: specialite.nom, value: specialite.nom}))}
+                    onChange={handleSpecialiteChange}
+                    placeholder="Select a specialite"
+                />
+            </div>
+            <div className="card mx-2">
+                <DataView value={restaurants} itemTemplate={itemTemplate} layout={layout} header={header()}/>
             </div>
         </>
     );
