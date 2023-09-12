bd8f494a103b2ade382bbe1362f2ff9021316e04
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-12 23:14:28 +0100

    product details done

--- src/clientcomponent/RestaurantProductDetails.js
+++ src/clientcomponent/RestaurantProductDetails.js
@@ -1,18 +1,19 @@
 import {Link, useParams} from 'react-router-dom';
 import axios from '../service/callerService';
 import {useEffect, useState, useRef} from "react";
-import {Card, CardContent, Rating} from "@mui/material";
-import {Col, Row} from "react-bootstrap";
+import { Rating} from "@mui/material";
 import React from "react";
 import {accountService} from "../service/accountService";
-import {Button} from 'primereact/button';
 import {Toast} from "primereact/toast";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import 'primeicons/primeicons.css';
 import "primereact/resources/primereact.min.css";
 import {Tag} from "primereact/tag";
 import Skeleton from "../skeleton/ProfileSkeleton"
-import Typography from "@mui/material/Typography";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import {Button} from 'primereact/button';
+
+
 
 
 export default function RestaurantProductDetails() {
@@ -140,74 +141,96 @@ export default function RestaurantProductDetails() {
         <>
             <Toast ref={toast}/>
 
-            <Card className="mt-4 mx-2" style={{backgroundColor: "whitesmoke"}}>
+            <Card className="mt-4 mx-2" style={{ backgroundColor: "whitesmoke" }}>
                 <CardContent>
-                    <Row>
-                        <Col sm={12} md={6} className="mb-3 mb-md-0">
-                            <div className="justify-content-center d-flex">
-                                <img alt="badr" src={products.photo} style={{
-                                    width: "70%",
-                                    height: "250px", objectFit: "fill", borderRadius: "10px"
-                                }}/>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <div className=" d-flex">
+                                <img
+                                    alt="badr"
+                                    src={products.photo}
+                                    style={{
+                                        width: "100%",
+                                        height: "auto",
+                                        maxHeight: "300px",
+                                        borderRadius: "10px",
+                                    }}
+                                />
                             </div>
-                        </Col>
-                        <Col sm={12} md={6}>
-                            <div className="map-container justify-content-start d-flex">
-                                <div className="flex flex-column align-items-start sm:align-items-start ">
+                        </Grid>
+                        <Grid item xs={12} md={6} className="md:flex md:flex-col md:items-start text-center text-md-start">
+                            <div className="map-container d-flex">
+                                <div className="flex flex-column ">
+                                    <Typography  gutterBottom>
+                                       <span className="font-extrabold text-2xl">Product Name: {products.nom}</span>
+                                    </Typography>
+                                    <Typography variant="body1" gutterBottom>
+                                        <span className="font-extrabold text-1xl">Product Description: </span>
+                                        <span className="font-monospace text-1xl"> {products.description}</span>
+                                    </Typography>
+                                    <Typography variant="body1" gutterBottom>
+                                        <span className="font-extrabold text-1xl">Stock: </span>
+                                        <span className="font-monospace text-1xl"> {products.stock} pcs</span>
+                                    </Typography>
+                                    <Typography variant="body1" gutterBottom>
+                                        <span className="font-extrabold text-1xl">Restaurant: </span>
+                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.nom ||''} </span>
+                                    </Typography>
+                                    <Typography variant="body1" gutterBottom>
+                                        <span className="font-extrabold text-1xl">Speciality: </span>
+                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.specialite.nom ||''} </span>
+                                    </Typography>
                                     <div >
-                                        <span className="text-1xl font-monospace text-900">Product Name :</span>
-                                        <span className="text-1xl font-bold text-900">{products.nom}</span>
-                                    </div>
-                                    <div className="mt-1">
-                                        <span className="text-1xl font-monospace text-900">Product Description :</span>
-                                        <span className="text-1xl font-bold text-900">{products.description}</span>
-                                    </div>
-                                    <div className="mt-1">
-                                        <span className="text-1xl font-monospace text-900">Stock :</span>
-                                        <span className="text-1xl font-bold text-900">{products.stock} Pcs</span>
-                                    </div>
+                                        <Typography variant="body1" gutterBottom>
+                                            <span className="font-extrabold text-1xl">Status: </span>
 
-                                    <div className="mt-1 flex">
-                                        <span className="text-1xl font-monospace text-900">Status :</span>
                                         {products.promotion === true ? (
-                                            <Tag value="On Sale" severity="danger" icon="pi pi-tag" />
-                                        ):(
-                                            <Tag value="New" severity="success" icon="pi pi-plus" />
-                                        )}
-                                    </div>
-                                    <div className="mt-1 flex">
-                                        <Rating value={getAverageRating(products)} readOnly cancel={false}  />
-                                        <Typography className="font-monospace ">({getReviews(products)})review</Typography>
-                                    </div>
-                                    <div className="d-flex justify-content-lg-between gap-3 align-items-start mt-3">
-                                        <span className="text-2xl font-semibold">{products.prix} Dh</span>
-
-                                        {productInCart[products.id] ? (
-                                            <Link to="/admin/cart">
-                                                <Button
-                                                    style={{background: 'linear-gradient(-225deg,#AC32E4 0%,#7918F2 48%,#4801FF 100%)'}}
-                                                    icon="pi pi-external-link"
-                                                    className="p-button-rounded mt-2"
-                                                    disabled={products.stock <= 0}
-                                                />
-                                            </Link>
+                                            <Tag className="mx-2"  value="On Sale" severity="danger" icon="pi pi-tag" />
                                         ) : (
-                                            <Button
-                                                icon="pi pi-shopping-cart"
-                                                className="p-button-rounded mt-2"
-                                                onClick={() => handleAddToCart(products)}
-                                                disabled={products.stock <= 0 || productInCart[products.id]}
-                                            />
+                                            <Tag className="mx-2"  value="New" severity="success" icon="pi pi-tag" />
                                         )}
+                                        </Typography>
                                     </div>
+                                    <div className="mb-1 ">
+
+                                        <Typography variant="body1" gutterBottom>
+                                            <div>
+                                                <Rating value={getAverageRating(products)} readOnly cancel={false} />
+                                                <small className=" text-black">({getReviews(products)}) review</small>
+                                            </div>
+                                        </Typography>
+                                    </div>
+                                    <div className="d-flex justify-content-between align-items-center ">
+                                        <Typography variant="h6">Price: {products.prix} Dh</Typography>
+                                        <div>
+                                            {productInCart[products.id] ? (
+                                                <Link to="/admin/cart">
+                                                    <Button
+                                                        style={{background: 'linear-gradient(-225deg,#AC32E4 0%,#7918F2 48%,#4801FF 100%)'}}
+                                                        icon="pi pi-external-link"
+                                                        className="p-button-rounded mt-2"
+                                                        disabled={products.stock <= 0}
+                                                    />
+                                                </Link>
+                                            ) : (
+                                                <Button
+                                                    icon="pi pi-shopping-cart"
+                                                    className="p-button-rounded mt-2"
+                                                    onClick={() => handleAddToCart(products)}
+                                                    disabled={products.stock <= 0 || productInCart[products.id]}
+                                                />
+                                            )}
+                                        </div>
+                                    </div>
+
                                 </div>
-
                             </div>
-                        </Col>
-                    </Row>
-
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </Card>
+
+
         </>
     );
 }
