380aac7cb71fad8c35ca107140080239da100927
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-13 13:56:13 +0100

    product details speciality

--- src/clientcomponent/RestaurantProductDetails.js
+++ src/clientcomponent/RestaurantProductDetails.js
@@ -13,6 +13,10 @@ import Skeleton from "../skeleton/ProfileSkeleton"
 import { Card, CardContent, Grid, Typography } from "@mui/material";
 import {Button} from 'primereact/button';
 
+import { Image } from 'primereact/image';
+
+
+
 
 
 
@@ -22,6 +26,9 @@ export default function RestaurantProductDetails() {
     const [userId, setUserId] = useState("");
     const toast = useRef(null);
     const [productInCart, setProductInCart] = useState({});
+    const [productSpeciality, setProductsSpeciality] = useState({});
+    const icon = (<i className="pi pi-check"></i>)
+
 
 
     const loadProductsUser = () => {
@@ -90,17 +97,28 @@ export default function RestaurantProductDetails() {
     };
 
 
-
-
     useEffect(() => {
         axios.get(`/api/controller/produits/${id}`)
             .then((response) => {
                 setProducts(response.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching product:", error);
             });
     }, [id]);
+
+    useEffect(() => {
+        // Define specialiteId after products have been set
+        const specialiteId = products.restaurant && products.restaurant.specialite.id;
+
+        // Check if specialiteId is defined before making the request
+        if (specialiteId !== undefined) {
+            axios.get(`/api/controller/produits/restaurant/speciality/${specialiteId}`)
+                .then((response) => {
+                    setProductsSpeciality(response.data);
+                })
+                .catch((error) => {
+                    console.error('Error fetching products with speciality:', error);
+                });
+        }
+    }, [products]);
 
 
 
@@ -132,6 +150,100 @@ export default function RestaurantProductDetails() {
 
 
 
+    const itemTemplate = (product) => {
+        if (!product) {
+            return;
+        }
+        return (
+            <div key={product.id} className={`col mb-4 ${product.stock <= 0 ? 'out-of-stock' : ''}`}>
+                <div className="card h-100">
+                    <div className="flex flex-column xl:flex-row xl:align-items-start p-2 gap-4">
+                        <Link to={`/admin/all_products/product/${product.id}`}>
+                            <div style={{position: 'relative'}}>
+                                <img className="w-90 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"
+                                     src={product.photo}
+                                     alt={product.nom}
+                                     style={{
+                                         width: '180px',
+                                         height: '140px',
+                                         borderRadius: '8px'
+                                     }}/>
+                                {product.stock <= 0 ? (
+                                    <Tag
+                                        severity="warning"
+                                        value="Out of Stock"
+                                        style={{
+                                            fontSize:"10px",
+                                            position: 'absolute',
+                                            top: '3px',
+                                            right: '11px',
+                                        }}
+                                    />
+                                ) : (
+                                    <Tag
+                                        severity="success"
+                                        value="In Stock"
+                                        style={{
+                                            fontSize:"10px",
+                                            position: 'absolute',
+                                            top: '3px',
+                                            right: '11px',
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        </Link>
+                        <div
+                            className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                                <div className="text-2xl font-bold text-900">{product.nom}</div>
+                                <Rating value={getAverageRating(product)} readOnly cancel={false}  ></Rating>
+                                <Typography className="font-monospace ">({getReviews(product)})review</Typography>
+                                <div className="flex align-items-center gap-3">
+                                    {product.promotion === true && (
+                                        <Tag value="On Sale" severity="danger" icon="pi pi-tag" />
+                                    )}
+                                    <span className="flex align-items-center gap-2">
+                                    <span className="font-semibold">{product.stock} Pcs</span>
+
+                                </span>
+                                </div>
+                                <span className="font-semibold">{product.restaurant && product.restaurant.specialite.nom} Pcs</span>
+                                <span className="font-semibold">{product.restaurant.nom } Pcs</span>
+                            </div>
+                            <div className="d-flex justify-content-lg-between gap-3 align-items-center mt-3">
+                                <span className="text-2xl font-semibold">{product.prix} Dh</span>
+
+                                {productInCart[product.id] ? (
+                                    <Link to="/admin/cart">
+                                        <Button
+                                            style={{background: 'linear-gradient(-225deg,#AC32E4 0%,#7918F2 48%,#4801FF 100%)'}}
+                                            icon="pi pi-external-link"
+                                            className="p-button-rounded mt-2"
+                                            disabled={product.stock <= 0}
+                                        />
+                                    </Link>
+                                ) : (
+                                    <Button
+                                        icon="pi pi-shopping-cart"
+                                        className="p-button-rounded mt-2"
+                                        onClick={() => handleAddToCart(product)}
+                                        disabled={product.stock <= 0 || productInCart[product.id]}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    const groupedProducts = [];
+    for (let i = 0; i < productSpeciality.length; i += 4) {
+        groupedProducts.push(productSpeciality.slice(i, i + 4));
+    }
+
 
 
 
@@ -141,20 +253,26 @@ export default function RestaurantProductDetails() {
         <>
             <Toast ref={toast}/>
 
-            <Card className="mt-4 mx-2" style={{ backgroundColor: "whitesmoke" }}>
+            <Card className="mt-4 mx-4" style={{ backgroundColor: "whitesmoke" }}>
                 <CardContent>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
-                            <div className=" d-flex">
-                                <img
+                    <Grid container spacing={2} >
+                        <Grid item xs={12} md={6} >
+                            <div>
+                                <Image
+
                                     alt="badr"
                                     src={products.photo}
-                                    style={{
-                                        width: "100%",
-                                        height: "auto",
-                                        maxHeight: "300px",
-                                        borderRadius: "10px",
+                                    indicatorIcon={icon}
+                                    preview
+
+                                    imageStyle={{
+                                        width: 350,
+                                        height: 250,
+                                        borderRadius: 10,
+                                        objectFit:"fill"
                                     }}
+
+
                                 />
                             </div>
                         </Grid>
@@ -174,11 +292,11 @@ export default function RestaurantProductDetails() {
                                     </Typography>
                                     <Typography variant="body1" gutterBottom>
                                         <span className="font-extrabold text-1xl">Restaurant: </span>
-                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.nom ||''} </span>
+                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.nom } </span>
                                     </Typography>
                                     <Typography variant="body1" gutterBottom>
                                         <span className="font-extrabold text-1xl">Speciality: </span>
-                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.specialite.nom ||''} </span>
+                                        <span className="font-monospace text-1xl"> {products.restaurant && products.restaurant.specialite.nom } </span>
                                     </Typography>
                                     <div >
                                         <Typography variant="body1" gutterBottom>
@@ -230,6 +348,13 @@ export default function RestaurantProductDetails() {
                 </CardContent>
             </Card>
 
+            <div className="container mt-5">
+                {groupedProducts.map((group) => (
+                    <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                        {group.map((product) => itemTemplate(product))}
+                    </div>
+                ))}
+            </div>
 
         </>
     );
