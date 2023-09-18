cfeb325f48631810ca08046c8504beb5c96ccc8b
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-18 21:58:51 +0100

    homepage icon contact slick

--- src/slick-Slider/ContactSlick.js
+++ src/slick-Slider/ContactSlick.js
@@ -4,11 +4,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/HomeImgSlick.css";
 import Typography from "@mui/material/Typography";
+import {Tag} from "primereact/tag";
 
 function ContactSlick() {
     const textItems = [
         { id: 1, text: "Text 1" },
-        { id: 2, text: "Text 2" },
+        { id: 2, text: "+212 0666995588",icon:"pi pi-phone" },
         { id: 3, text: "Text 3" },
         { id: 4, text: "Text 4" },
     ];
@@ -27,9 +28,10 @@ function ContactSlick() {
 
     return (
         <div className="carousel-container">
-            <Slider {...settings}>
+            <Slider {...settings} >
                 {textItems.map((item) => (
-                    <div key={item.id} className="slide">
+                    <div key={item.id} className="slide bg-black text-white" >
+                        {/*<Tag icon={item.icon}/>*/}
                         <Typography>{item.text}</Typography>
                     </div>
                 ))}
