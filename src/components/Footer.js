b4a7244d85cb8bcb593f8acbbab358549c19fc3b
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-18 20:52:10 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,32 +7,32 @@ import "../styles/footer.css"
 
 export default function Footer() {
     return (
-        <div className="footer-dark" style={{marginTop:"50px",}}>
+        <div className="footer-dark">
             <footer>
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6 item text">
-                            <h3>About Our App</h3>
-                            <p>Our Restaurant Location App helps you find the nearest Restaurant in your area quickly and easily.</p>
-                            <p>With detailed information on each Restaurant, including hours of operation, contact information, and services offered, our app makes it easy to find the right Restaurant for your needs.</p>
-                        </div>
+                        {/*<div className="col-md-6 item text">*/}
+                        {/*    <h3>About Our App</h3>*/}
+                        {/*    <p>Our Restaurant Location App helps you find the nearest Restaurant in your area quickly and easily.</p>*/}
+                        {/*    <p>With detailed information on each Restaurant, including hours of operation, contact information, and services offered, our app makes it easy to find the right Restaurant for your needs.</p>*/}
+                        {/*</div>*/}
 
-                        <div className="col-sm-6 col-md-3 item">
-                            <h3>About Us</h3>
-                            <ul>
-                                <li><h5 >Company</h5></li>
-                                <li><h5>Team</h5></li>
-                                <li><h5 >Careers</h5></li>
-                            </ul>
-                        </div>
-                        <div className="col-sm-6 col-md-3 item">
-                            <h3>Contact Us</h3>
-                            <ul>
-                                <li><h6 >Phone: 555-123-4567</h6></li>
-                                <li><h6 >Email: info@restaurantlocationapp.com</h6></li>
-                                <li><h6 >Address: 123 Main St, Morocco</h6></li>
-                            </ul>
-                        </div>
+                        {/*<div className="col-sm-6 col-md-3 item">*/}
+                        {/*    <h3>About Us</h3>*/}
+                        {/*    <ul>*/}
+                        {/*        <li><h5 >Company</h5></li>*/}
+                        {/*        <li><h5>Team</h5></li>*/}
+                        {/*        <li><h5 >Careers</h5></li>*/}
+                        {/*    </ul>*/}
+                        {/*</div>*/}
+                        {/*<div className="col-sm-6 col-md-3 item">*/}
+                        {/*    <h3>Contact Us</h3>*/}
+                        {/*    <ul>*/}
+                        {/*        <li><h6 >Phone: 555-123-4567</h6></li>*/}
+                        {/*        <li><h6 >Email: info@restaurantlocationapp.com</h6></li>*/}
+                        {/*        <li><h6 >Address: 123 Main St, Morocco</h6></li>*/}
+                        {/*    </ul>*/}
+                        {/*</div>*/}
                         <div className="col item social">
                             <a href="https://web.telegram.org/z/"><RiTelegramFill/></a>
                             <a href="https://web.telegram.org/z/"><RiFacebookCircleFill/></a>
