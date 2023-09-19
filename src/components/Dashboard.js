eb0ee61c94fd463c5001c04b37711db0d822c384
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-19 19:04:28 +0100

    admin dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,128 @@
-import React from "react"
+import React, {useState} from "react"
+import { Chart } from 'primereact/chart';
+
 
 export default function Dashboard(){
+
+    const [chartData] = useState({
+        datasets: [{
+            data: [
+                11,
+                16,
+                7,
+                3,
+                14,
+                12,
+                10
+            ],
+            backgroundColor: [
+                "#42A5F5",
+                "#66BB6A",
+                "#FFA726",
+                "#26C6DA",
+                "#7E57C2"
+            ],
+            label: 'My dataset'
+        }],
+        labels: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Grey",
+            "Blue"
+        ]
+    });
+
+    const [lightOptions] = useState({
+        plugins: {
+            legend: {
+                labels: {
+                    color: '#495057'
+                }
+            }
+        },
+        scales: {
+            r: {
+                grid: {
+                    color: '#ebedef'
+                }
+            }
+        }
+    });
     return(
-        <div>
-            <h1>this is the Admin's dashboard</h1>
+        <>
+        <div className="card mt-5 p-1 m-2">
+
+            <div className="grid">
+                <div className="col-12 md:col-6 lg:col-3">
+                    <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+                        <div className="flex justify-content-between mb-3">
+                            <div>
+                                <span className="block text-500 font-medium mb-3">Orders</span>
+                                <div className="text-900 font-medium text-xl">152</div>
+                            </div>
+                            <div className="flex align-items-center justify-content-center bg-blue-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
+                                <i className="pi pi-shopping-cart text-blue-500 text-xl"></i>
+                            </div>
+                        </div>
+                        <span className="text-green-500 font-medium">24 new </span>
+                        <span className="text-500">since last visit</span>
+                    </div>
+                </div>
+                <div className="col-12 md:col-6 lg:col-3">
+                    <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+                        <div className="flex justify-content-between mb-3">
+                            <div>
+                                <span className="block text-500 font-medium mb-3">Revenue</span>
+                                <div className="text-900 font-medium text-xl">$2.100</div>
+                            </div>
+                            <div className="flex align-items-center justify-content-center bg-orange-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
+                                <i className="pi pi-map-marker text-orange-500 text-xl"></i>
+                            </div>
+                        </div>
+                        <span className="text-green-500 font-medium">%52+ </span>
+                        <span className="text-500">since last week</span>
+                    </div>
+                </div>
+                <div className="col-12 md:col-6 lg:col-3">
+                    <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+                        <div className="flex justify-content-between mb-3">
+                            <div>
+                                <span className="block text-500 font-medium mb-3">Customers</span>
+                                <div className="text-900 font-medium text-xl">28441</div>
+                            </div>
+                            <div className="flex align-items-center justify-content-center bg-cyan-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
+                                <i className="pi pi-inbox text-cyan-500 text-xl"></i>
+                            </div>
+                        </div>
+                        <span className="text-green-500 font-medium">520  </span>
+                        <span className="text-500">newly registered</span>
+                    </div>
+                </div>
+                <div className="col-12 md:col-6 lg:col-3">
+                    <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+                        <div className="flex justify-content-between mb-3">
+                            <div>
+                                <span className="block text-500 font-medium mb-3">Comments</span>
+                                <div className="text-900 font-medium text-xl">152 Unread</div>
+                            </div>
+                            <div className="flex align-items-center justify-content-center bg-purple-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
+                                <i className="pi pi-comment text-purple-500 text-xl"></i>
+                            </div>
+                        </div>
+                        <span className="text-green-500 font-medium">85 </span>
+                        <span className="text-500">responded</span>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+            <div className="card flex justify-content-center mt-5 p-1 m-2">
+                <Chart type="polarArea" data={chartData} options={lightOptions} style={{ position: 'relative', width: '40%' }} />
+            </div>
+
+
+            </>
     );
 }
