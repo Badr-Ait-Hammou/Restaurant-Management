82347d910b963efd829f3df8c42137416f32be31
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-19 13:28:39 +0100

    homepage  style

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -37,7 +37,7 @@ export default function HomePage() {
             setProductsno(response.data);
         });
         loadProductsUser();
-        setLoading(true);
+        setLoading(false);
     }, []);
 
 
