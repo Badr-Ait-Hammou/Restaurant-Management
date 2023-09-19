803bf9f5888973e05d36f85fb008159cd41b8db5
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-19 20:41:39 +0100

    radar chart done

--- src/chart/RadarChart.js
+++ src/chart/RadarChart.js
@@ -4,99 +4,97 @@ import axios from '../service/callerService';
 
 const RadarChart = () => {
     const [chartData, setChartData] = useState({
-            labels: [],
-            datasets: [
-                {
-                    label: 'Average Restaurant Rating',
-                    backgroundColor: 'rgba(179,181,198,0.2)',
-                    borderColor: 'rgba(179,181,198,1)',
-                    pointBackgroundColor: 'rgba(179,181,198,1)',
-                    pointBorderColor: '#fff',
-                    pointHoverBackgroundColor: '#fff',
-                    pointHoverBorderColor: 'rgba(179,181,198,1)',
-                    data: [], // Average ratings
-                },
-                {
-                    label: 'Product Count',
-                    backgroundColor: 'rgba(255,99,132,0.2)',
-                    borderColor: 'rgba(255,99,132,1)',
-                    pointBackgroundColor: 'rgba(255,99,132,1)',
-                    pointBorderColor: '#fff',
-                    pointHoverBackgroundColor: '#fff',
-                    pointHoverBorderColor: 'rgba(255,99,132,1)',
-                    data: [], // Product counts
-                },
-            ],
-        });
+        labels: [],
+        datasets: [
+            {
+                label: 'Average Restaurant Rating',
+                backgroundColor: 'rgba(179,181,198,0.2)',
+                borderColor: 'rgba(179,181,198,1)',
+                pointBackgroundColor: 'rgba(179,181,198,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(179,181,198,1)',
+                data: [],
+            },
+            {
+                label: 'Product Count',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                pointBackgroundColor: 'rgba(255,99,132,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(255,99,132,1)',
+                data: [],
+            },
+        ],
+    });
 
-        useEffect(() => {
-        // Fetch data from the API
+    useEffect(() => {
         axios
-            .get('/api/controller/orders/all')
+            .get('/api/controller/restaurants/')
             .then((response) => {
-                const ordersData = response.data;
+                const restaurantsData = response.data;
 
-                // Calculate average ratings and product counts for each restaurant
-                const restaurantRatings = {};
-                const restaurantProductCounts = {};
+                const restaurantNames = restaurantsData.map((restaurant) => `${restaurant.nom} (${restaurant.id})`);
+                const productCounts = restaurantsData.map((restaurant) => restaurant.produitList.length);
 
-                ordersData.forEach((order) => {
-                    const restaurantName = order.produit.restaurant.nom;
-                    const avisList = order.produit.avisList;
-                    const totalRating = avisList.reduce((acc, avis) => acc + avis.rating, 0);
-                    // const averageRating = avisList.length > 0 ? totalRating / avisList.length : 0;
+                axios
+                    .get('/api/controller/orders/all')
+                    .then((response) => {
+                        const ordersData = response.data;
 
-                    if (!restaurantRatings[restaurantName]) {
-                        restaurantRatings[restaurantName] = {
-                            totalRating,
-                            count: avisList.length,
-                        };
-                    } else {
-                        restaurantRatings[restaurantName].totalRating += totalRating;
-                        restaurantRatings[restaurantName].count += avisList.length;
-                    }
+                        const restaurantRatings = {};
 
-                    // Count products
-                    if (!restaurantProductCounts[restaurantName]) {
-                        restaurantProductCounts[restaurantName] = 1;
-                    } else {
-                        restaurantProductCounts[restaurantName]++;
-                    }
-                });
+                        ordersData.forEach((order) => {
+                            const restaurantName = order.produit.restaurant.nom;
+                            const avisList = order.produit.avisList;
+                            const totalRating = avisList.reduce((acc, avis) => acc + avis.rating, 0);
+                            // const averageRating = avisList.length > 0 ? totalRating / avisList.length : 0;
 
-                const restaurantNames = Object.keys(restaurantRatings);
-                const averageRatings = restaurantNames.map(
-                    (restaurantName) =>
-                        restaurantRatings[restaurantName].totalRating /
-                        restaurantRatings[restaurantName].count
-                );
-                const productCounts = restaurantNames.map((restaurantName) => restaurantProductCounts[restaurantName]);
+                            if (!restaurantRatings[restaurantName]) {
+                                restaurantRatings[restaurantName] = {
+                                    totalRating,
+                                    count: avisList.length,
+                                };
+                            } else {
+                                restaurantRatings[restaurantName].totalRating += totalRating;
+                                restaurantRatings[restaurantName].count += avisList.length;
+                            }
+                        });
 
-                setChartData({
-                    labels: restaurantNames,
-                    datasets: [
-                        {
-                            label: 'Average Restaurant Rating',
-                            backgroundColor: 'rgba(179,181,198,0.2)',
-                            borderColor: 'rgba(179,181,198,1)',
-                            pointBackgroundColor: 'rgba(179,181,198,1)',
-                            pointBorderColor: '#fff',
-                            pointHoverBackgroundColor: '#fff',
-                            pointHoverBorderColor: 'rgba(179,181,198,1)',
-                            data: averageRatings,
-                        },
-                        {
-                            label: 'Product Count',
-                            backgroundColor: 'rgba(255,99,132,0.2)',
-                            borderColor: 'rgba(255,99,132,1)',
-                            pointBackgroundColor: 'rgba(255,99,132,1)',
-                            pointBorderColor: '#fff',
-                            pointHoverBackgroundColor: '#fff',
-                            pointHoverBorderColor: 'rgba(255,99,132,1)',
-                            data: productCounts,
-                        },
-                    ],
-                });
+                        const averageRatings = Object.values(restaurantRatings).map(
+                            (restaurantRating) => restaurantRating.totalRating / restaurantRating.count
+                        );
+
+                        setChartData({
+                            labels: restaurantNames,
+                            datasets: [
+                                {
+                                    label: 'Average Restaurant Rating',
+                                    backgroundColor: 'rgba(179,181,198,0.2)',
+                                    borderColor: 'rgba(179,181,198,1)',
+                                    pointBackgroundColor: 'rgba(179,181,198,1)',
+                                    pointBorderColor: '#fff',
+                                    pointHoverBackgroundColor: '#fff',
+                                    pointHoverBorderColor: 'rgba(179,181,198,1)',
+                                    data: averageRatings,
+                                },
+                                {
+                                    label: 'Product Count',
+                                    backgroundColor: 'rgba(255,99,132,0.2)',
+                                    borderColor: 'rgba(255,99,132,1)',
+                                    pointBackgroundColor: 'rgba(255,99,132,1)',
+                                    pointBorderColor: '#fff',
+                                    pointHoverBackgroundColor: '#fff',
+                                    pointHoverBorderColor: 'rgba(255,99,132,1)',
+                                    data: productCounts,
+                                },
+                            ],
+                        });
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching data:', error);
+                    });
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
@@ -127,12 +125,12 @@ const RadarChart = () => {
     };
 
     return (
-            <Chart
-                type="radar"
-                data={chartData}
-                options={lightOptions}
-            />
+        <Chart
+            type="radar"
+            data={chartData}
+            options={lightOptions}
+        />
     );
 };
 
-    export default RadarChart;
+export default RadarChart;
