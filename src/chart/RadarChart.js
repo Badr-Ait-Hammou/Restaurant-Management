549c1aad9e7a626417c5c65683d1f912922c6971
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-09-20 20:22:37 +0100

    radar chart issue solved

--- src/chart/RadarChart.js
+++ src/chart/RadarChart.js
@@ -29,75 +29,50 @@ const RadarChart = () => {
         ],
     });
 
-
-
     useEffect(() => {
         axios
             .get('/api/controller/restaurants/')
             .then((response) => {
                 const restaurantsData = response.data;
 
+                // const restaurantNames = restaurantsData.map((restaurant) => `${restaurant.nom} (${restaurant.id})`);
+                // const productCounts = restaurantsData.map((restaurant) => restaurant.produitList.length);
+                //
+                //
+                // // Use the getAverageRating method here to calculate ratings for each restaurant
+                // const restaurantRatings = restaurantsData.map((restaurant) => getAverageRating(restaurant.produitList));
+
                 const restaurantNames = restaurantsData.map((restaurant) => `${restaurant.nom} (${restaurant.id})`);
                 const productCounts = restaurantsData.map((restaurant) => restaurant.produitList.length);
-
-                axios
-                    .get('/api/controller/orders/')
-                    .then((response) => {
-                        const ordersData = response.data;
-
-                        const restaurantRatings = {};
-
-                        ordersData.forEach((order) => {
-                            const restaurantName = order.produit.restaurant.nom;
-                            const avisList = order.produit.avisList;
-                            const totalRating = avisList.reduce((acc, avis) => acc + avis.rating, 0);
-                            //const averageRating = avisList.length > 0 ? totalRating / avisList.length : 0;
-
-                            if (!restaurantRatings[restaurantName]) {
-                                restaurantRatings[restaurantName] = {
-                                    totalRating,
-                                    count: avisList.length,
-                                };
-                            } else {
-                                restaurantRatings[restaurantName].totalRating += totalRating;
-                                restaurantRatings[restaurantName].count += avisList.length;
-                            }
-                        });
-
-                        const averageRatings = Object.values(restaurantRatings).map(
-                            (restaurantRating) => restaurantRating.totalRating / restaurantRating.count
-                        );
-
-
-                        setChartData({
-                            labels: restaurantNames,
-                            datasets: [
-                                {
-                                    label: 'Average Restaurant Rating',
-                                    backgroundColor: 'rgba(34,169,182,0.52)',
-                                    borderColor: 'rgba(20,115,128,0.85)',
-                                    pointBackgroundColor: 'rgba(179,181,198,1)',
-                                    pointBorderColor: '#fff',
-                                    pointHoverBackgroundColor: '#fff',
-                                    pointHoverBorderColor: 'rgba(179,181,198,1)',
-                                    data: averageRatings,
-                                },
-                                {
-                                    label: 'Product Count ',
-                                    backgroundColor: 'rgba(255,99,132,0.2)',
-                                    borderColor: 'rgba(255,99,132,1)',
-                                    pointBackgroundColor: 'rgba(255,99,132,1)',
-                                    pointBorderColor: '#fff',
-                                    pointHoverBackgroundColor: '#fff',
-                                    pointHoverBorderColor: 'rgba(255,99,132,1)',
-                                    data: productCounts,
-                                },
-                            ],
-                        });
-                    })
-                    .catch((error) => {
-                        console.error('Error fetching data:', error);
-                    });
+                const restaurantRatings = restaurantsData.map((restaurant) => {
+                    const averageRating = calculateAverageRating(restaurant.produitList);
+                    return averageRating;
+                });
+                setChartData({
+                    labels: restaurantNames,
+                    datasets: [
+                        {
+                            label: 'Average Restaurant Rating',
+                            backgroundColor: 'rgba(34,169,182,0.52)',
+                            borderColor: 'rgba(20,115,128,0.85)',
+                            pointBackgroundColor: 'rgba(179,181,198,1)',
+                            pointBorderColor: '#fff',
+                            pointHoverBackgroundColor: '#fff',
+                            pointHoverBorderColor: 'rgba(179,181,198,1)',
+                            data: restaurantRatings,
+                        },
+                        {
+                            label: 'Product Count ',
+                            backgroundColor: 'rgba(255,99,132,0.2)',
+                            borderColor: 'rgba(255,99,132,1)',
+                            pointBackgroundColor: 'rgba(255,99,132,1)',
+                            pointBorderColor: '#fff',
+                            pointHoverBackgroundColor: '#fff',
+                            pointHoverBorderColor: 'rgba(255,99,132,1)',
+                            data: productCounts,
+                        },
+                    ],
+                });
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
@@ -125,8 +100,28 @@ const RadarChart = () => {
                 },
             },
         },
-
     };
+
+    const calculateAverageRating = (products) => {
+        if (products.length === 0) {
+            return 0;
+        }
+
+        let totalRating = 0;
+        let totalRatings = 0;
+
+        products.forEach((product) => {
+            const avisList = product.avisList;
+            if (avisList.length > 0) {
+                const productRating = avisList.reduce((acc, avis) => acc + avis.rating, 0);
+                totalRating += productRating;
+                totalRatings += avisList.length;
+            }
+        });
+
+        return totalRating / (totalRatings );
+    };
+
 
     return (
         <Chart
@@ -138,6 +133,7 @@ const RadarChart = () => {
 };
 
 export default RadarChart;
+
 //
 // import React, { useState, useEffect } from 'react';
 // import { Chart } from 'primereact/chart';
@@ -233,7 +229,7 @@ export default RadarChart;
 //                         const averageRatings = Object.values(restaurantRatings).map(
 //                             (restaurantRating) => restaurantRating.totalRating / restaurantRating.count
 //                         );
-//
+
 //                         setChartData({
 //                             labels: chartLabels,
 //                             datasets: [
